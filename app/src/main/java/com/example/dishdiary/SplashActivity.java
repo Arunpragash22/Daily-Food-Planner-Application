435ebd74f3275c2139e503f7804cc7e02c342dde
Dell <vimalanarunpragash2000@gmail.com>
2025-08-11 14:04:30 +0530

    initial push

--- app/src/main/java/com/example/dishdiary/SplashActivity.java
+++ app/src/main/java/com/example/dishdiary/SplashActivity.java
@@ -11,7 +11,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.airbnb.lottie.LottieAnimationView;
-
+//create splash screen
 public class SplashActivity extends AppCompatActivity {
 
     @Override
